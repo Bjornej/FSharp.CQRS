818de9bb10b5621c8ffb225f6892528cae415caf
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-10-23 23:16:27 +0200

    Added Conversation type and commands

---
+++ Source/Domain/Commands.fs
@@ -0,0 +1,26 @@
+﻿module Commands
+open System;
+
+type CreateConversation = {
+    SenderId:int
+    Title: string
+    Message:string
+    TimeStamp:DateTime
+}
+
+type RespondToConversation = {
+    SenderId:int
+    Message:string
+    TimeStamp:DateTime
+}
+
+type ReplyToConversation = {
+    SenderId:int
+    Message:string
+    TimeStamp:DateTime
+}
+
+type ConversationCommand = 
+    | CreateConversation
+    | RespondToConversation
+    | ReplyToConversation
---
+++ Source/Domain/Conversation.fs
@@ -0,0 +1,38 @@
+﻿module Conversation
+open System
+
+type Request = {
+    Message:string
+    TimeStamp : DateTime
+}
+
+type Response = {
+    Message:string
+    TimeStamp : DateTime
+    ResponderId:int
+}
+
+type Reply = {
+    Text:string
+    TimeStamp: DateTime
+}
+
+type Message = 
+    | Request
+    | Response
+    | Reply
+
+type EstimatedResponseTime = 
+    | None
+    | Estimate of DateTime
+
+type Conversation = {
+    Id:int
+    Version: int
+    OwnerId:int
+    Title:string
+    Sender : int
+    Messages: List<Message>
+    Deadline: EstimatedResponseTime
+}
+ 
