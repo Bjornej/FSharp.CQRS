8064f1dcd7106a0e0a32777ed9f20043c179ce68
Paolo Nicodemo <paolonicodemo@gmail.com>
2014-10-26 17:45:02 +0100

    Added event handling

--- Source/Domain/Conversation.fs
+++ Source/Domain/Conversation.fs
@@ -1,8 +1,9 @@
 ﻿module Conversation
-open System
+open System;
+open Events;
 
 type Request = {
-    Message:string
+    Request:string
     TimeStamp : DateTime
 }
 
@@ -18,21 +19,66 @@ type Reply = {
 }
 
 type Message = 
-    | Request
-    | Response
-    | Reply
+    | Request of Request
+    | Response of Response
+    | Reply of Reply
 
 type EstimatedResponseTime = 
     | None
     | Estimate of DateTime
 
-type Conversation = {
-    Id:int
-    Version: int
-    OwnerId:int
-    Title:string
-    Sender : int
-    Messages: List<Message>
-    Deadline: EstimatedResponseTime
-}
+type Conversation = 
+    { Id:int
+      mutable Version: int
+      OwnerId:int
+      Title:string
+      Sender : int
+      Messages: List<Message>
+      Deadline: EstimatedResponseTime }
+
+    member x.Raise(evt:ConversationEvent) = 
+        x.Apply(evt:ConversationEvent)
+
+    member x.Apply(evt:ConversationEvent) = 
+        match evt with
+        | Created(z)-> 
+            let request = {
+                Request = z.Message;
+                TimeStamp=z.TimeStamp
+            }
+            let newConversation = {
+                Id= 0;
+                Version=1;
+                OwnerId=z.SenderId;
+                Title = z.Title;
+                Sender = z.SenderId;
+                Messages = Request(request)::[];
+                Deadline = Estimate(z.TimeStamp.AddMinutes(30.0))
+            }
+            newConversation
+        | Responded(z) ->
+            let response = {    Message=z.Message; TimeStamp = z.TimeStamp;  ResponderId=z.SenderId}
+            let newConversation = {
+               Id= x.Id;
+               Version=x.Version+1;
+               OwnerId=x.OwnerId;
+               Title = x.Title;
+               Sender = x.Sender;
+               Messages =  Response(response) :: x.Messages;
+               Deadline = None
+            }
+            newConversation
+        | Replied(z) ->
+            let reply = { Text=z.Message; TimeStamp = z.TimeStamp; }
+            let newConversation = {
+               Id= x.Id;
+               Version=x.Version+1;
+               OwnerId=x.OwnerId;
+               Title = x.Title;
+               Sender = x.Sender;
+               Messages =  Reply(reply) :: x.Messages;
+               Deadline = Estimate(z.TimeStamp.AddMinutes(30.0))
+            }
+            newConversation           
+
  
---
+++ Source/Domain/Events.fs
@@ -0,0 +1,26 @@
+﻿module Events
+open System;
+
+type ConversationCreated = {
+    SenderId:int
+    Title: string
+    Message:string
+    TimeStamp:DateTime
+}
+
+type ConversationResponded = {
+    SenderId:int
+    Message:string
+    TimeStamp:DateTime
+}
+
+type ConversationReplied = {
+    SenderId:int
+    Message:string
+    TimeStamp:DateTime
+}
+
+type ConversationEvent = 
+    | Created of ConversationCreated
+    | Responded of ConversationResponded
+    | Replied of ConversationReplied
